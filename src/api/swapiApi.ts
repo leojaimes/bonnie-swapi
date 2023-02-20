6f1cfbefa00a77e5a96f79d7fdbdc166e2b43202
leo.jaimes <leo777jaimes@gmail.com>
2023-02-20 17:09:52 -0600

    upload

--- src/api/swapiApi.ts
+++ src/api/swapiApi.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
 
-export const swapiApi = axios.create({
-    baseURL: 'https://swapi.dev/api'
-})
+// export const swapiApi = axios.create({
+//     baseURL: 'https://swapi.dev/api'
+// })
+const baseURL = 'https://swapi.dev/api'
+const swapiApi = axios.create({
+    baseURL: baseURL
+});
+
+export default swapiApi;
+
--- src/components/people/InfinitePeople.tsx
+++ src/components/people/InfinitePeople.tsx
@@ -1,21 +1,38 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
 import InfiniteScroll from "react-infinite-scroller";
 import { getPeople } from "../../api/requests";
+import { PeopleResult, Person, Result } from '../../types/people_results';
+import { PersonComponent } from './PersonComponent';
 
 export const InfinitePeople = () => {
 
-    const fetchProjects = async ({ pageParam = 0 }) => {
-        const res = await fetch('/api/projects?cursor=' + pageParam)
-        return res.json()
-    }
-
-    const { data, fetchNextPage } = useInfiniteQuery({
+    const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
         queryKey: ['projects'],
-        queryFn: ({ pageParam = 1 }) => { return getPeople(pageParam) }, //siempre debe retornar la funcion de la consulta
-        getNextPageParam: (lastPage, pages) => lastPage.next, //la proxima consulta que tiene que hacer pero esta dentro del return de la queryFn
+        queryFn: getPeople, //siempre debe retornar la funcion de la consulta
+        getNextPageParam: (lastPage, pages) => lastPage.next || undefined, //la proxima consulta que tiene que hacer pero esta dentro del return de la queryFn
     })
 
+    if (isLoading) {
+        return (
+            <div className='loading'>Loading...</div>
+        )
+    }
     return (
-        <InfiniteScroll loadMore={() => { }} />
+        <InfiniteScroll loadMore={() => fetchNextPage()} hasMore={hasNextPage}>
+            {
+                data?.pages.map((item: PeopleResult, index) => (
+                    <>
+                        {
+                            item.results.map((personData: Result, index) => (
+                                <PersonComponent
+                                    key={index}
+                                    person={{ name: personData.name, eyeColor: personData.eye_color, hairColor: personData.hair_color }}
+                                ></PersonComponent>
+                            ))
+                        }
+                    </>
+                ))
+            }
+        </InfiniteScroll>
     )
 }
