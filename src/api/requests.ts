a31522186bc0c2cbc2a399453a7cb43207d27a44
leo.jaimes <leo777jaimes@gmail.com>
2023-02-20 18:23:29 -0600

    upload

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -2,8 +2,7 @@ import { PeopleResult } from '../types/people_results';
 import swapiApi from './swapiApi';
 import { SpeciesResult } from '../types/species_result';
 
-export const getPeople = async ({ pageParam = 1 }): Promise<PeopleResult> => {
-
+export const getPeople = async (pageParam = 1): Promise<PeopleResult> => {
     const res = await swapiApi.get<PeopleResult>(`/people?page=${pageParam}`)
     return res.data
 }
--- src/components/people/InfinitePeople.tsx
+++ src/components/people/InfinitePeople.tsx
@@ -1,4 +1,5 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
+import { AxiosError } from 'axios';
 import InfiniteScroll from "react-infinite-scroller";
 import { getPeople } from "../../api/requests";
 import { PeopleResult, Person, Result } from '../../types/people_results';
@@ -6,10 +7,16 @@ import { PersonComponent } from './PersonComponent';
 
 export const InfinitePeople = () => {
 
-    const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
+    const { data, fetchNextPage, hasNextPage, isLoading, isError, error, isFetching } = useInfiniteQuery({
         queryKey: ['projects'],
-        queryFn: getPeople, //siempre debe retornar la funcion de la consulta
-        getNextPageParam: (lastPage, pages) => lastPage.next || undefined, //la proxima consulta que tiene que hacer pero esta dentro del return de la queryFn
+        queryFn: ({ pageParam }) => getPeople(pageParam), //siempre debe retornar la funcion de la consulta
+        getNextPageParam: (lastPage, pages) => {
+            //esta funcion debe devolver el proximo parametro para hacer la consulta
+            const url = new URL(lastPage.next);
+            const searchParams = url.searchParams;
+            return searchParams.get('page') || '1'
+        },
+        onError: (err: AxiosError) => err,
     })
 
     if (isLoading) {
@@ -17,22 +24,29 @@ export const InfinitePeople = () => {
             <div className='loading'>Loading...</div>
         )
     }
+    if (isError) {
+        return (
+            <h1>{error.message}</h1>
+        )
+    }
     return (
-        <InfiniteScroll loadMore={() => fetchNextPage()} hasMore={hasNextPage}>
-            {
-                data?.pages.map((item: PeopleResult, index) => (
-                    <>
-                        {
-                            item.results.map((personData: Result, index) => (
-                                <PersonComponent
-                                    key={index}
-                                    person={{ name: personData.name, eyeColor: personData.eye_color, hairColor: personData.hair_color }}
-                                ></PersonComponent>
-                            ))
-                        }
-                    </>
-                ))
-            }
-        </InfiniteScroll>
+        <>
+            {isFetching && <div className='loading'>Fetching...</div>}
+            <InfiniteScroll loadMore={() => fetchNextPage()} hasMore={hasNextPage}>
+                {
+                    data?.pages.map((item: PeopleResult, index) => {
+                        return item.results.map((personData: Result, index) => (
+                            <PersonComponent
+                                key={index}
+                                person={{ name: personData.name, eyeColor: personData.eye_color, hairColor: personData.hair_color }}
+                            ></PersonComponent>
+                        ))
+                    })
+                }
+            </InfiniteScroll>
+
+
+        </>
+
     )
 }
